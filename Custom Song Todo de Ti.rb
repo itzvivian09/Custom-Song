3b6a5f083657ca097956a8ddb5a282d25e3f35b0
itzvivian09 <cheetoburrito213@gmail.com>
2026-03-03 17:56:34 -0800

    Custom Song Todo de Ti.rb

--- Custom Song Todo de Ti.rb
+++ Custom Song Todo de Ti.rb
@@ -1,58 +1,56 @@
 use_bpm 115
 use_synth :prophet
 
-# VARIABLES
-melody = "/Users/vivianvargas/Downloads/titi mi pregunto.mp3"
+# VARIABLES TO STORE SAMPLES
+middle="/Users/vivianvargas/Downloads/titi mi pregunto.mp3"
 
-# FUNCTION – melody pattern
+# FUNCTION
 define :melody_pattern do
-  play :bb4, amp: 0.5
+  play :bb4,amp: 0.5
   sleep 0.5
   play :eb4, amp: 0.5
   sleep 0.5
-  play :gb4, amp: 0.5
+  play :gb4,amp: 0.5
   sleep 0.5
-  play :eb4, amp: 0.5
-  sleep 0.5
+  play :eb4,amp: 0.5
+  
 end
-
 live_loop :cybol do
-  8.times do
+  20.times do
     sample :drum_cymbal_closed, amp: 0.6
-    sleep 1
+    sleep 0.5
     sample :drum_cymbal_pedal, amp: 0.5
-    sleep 1
+    sleep 0.5
   end
   stop
 end
-
+sleep 4
 live_loop :drums do
-  sleep 4
-  4.times do
-    sample :bd_haus, amp: 1.5
+  8.times do
+    sample :bd_haus, amp: 2
     sleep 2
   end
   stop
 end
-
 live_loop :melody do
   sleep 8
   4.times do
     melody_pattern
+    sleep 0.5
   end
   stop
 end
-
+live_loop :middle do
+  sleep 8
+  sample middle, amp: 0.75, beat_stretch: 17
+  stop
+end
 
 live_loop :ending do
   sleep 16
   
-  1.times do
-    melody_pattern
-  end
-  
   4.times do
-    sample :drum_cymbal_closed, amp: 0.7
+    sample :drum_cymbal_closed, amp: 1
     sleep 0.25
   end
   
